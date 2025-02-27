bd86e9599e0b1befab2d7fab6bb3174b41c157a1
MakWigglz <wigglz734@gmail.com>
2025-02-27 22:27:20 +0200

    Update and rename __init__.py to board_game_project/board_game/__init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# board_game/__init__.py
-
-from .board import Board
-from .dice import Dice
-from .token import Token
-from .content import Content
-
-__all__ = ['Board', 'Dice', 'Token', 'Content']
